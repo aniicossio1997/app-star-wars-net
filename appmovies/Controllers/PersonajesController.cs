6699b228b172e2543e9a8d7c1bd700ecc2993c1f
ana cossio <anacossio7@gmail.com>
2022-03-03 20:56:19 -0300

    se agrego la vista y el controlador de personajes

---
+++ appmovies/Controllers/PersonajesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace appmovies.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
